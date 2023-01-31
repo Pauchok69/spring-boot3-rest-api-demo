b7a85907899ca2d420e567b17c5256ea60a66c94
Andy <andrew.zerko@gmail.com>
2023-01-31 18:07:48 +0200

    Add Student entity and controller

---
+++ src/main/java/com/andy/springboot3restapidemo/controller/StudentController.java
@@ -0,0 +1,36 @@
+package com.andy.springboot3restapidemo.controller;
+
+import com.andy.springboot3restapidemo.bean.Student;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class StudentController {
+    @GetMapping("student")
+    public Student getStudent() {
+        return new Student(1, "John", "Doe");
+    }
+
+    @GetMapping("students")
+    public List<Student> getStudents() {
+        ArrayList<Student> students = new ArrayList<>();
+        students.add(new Student(1, "John", "Doe"));
+        students.add(new Student(2, "Jane", "Doe"));
+        students.add(new Student(3, "Boris", "Doe"));
+
+        return students;
+    }
+
+    @GetMapping("students/{id}/{first-name}/{last-name}")
+    public Student getStudentByPathVariable(
+            @PathVariable("id") int studentId,
+            @PathVariable("first-name") String studentFirstName,
+            @PathVariable("last-name") String studentLastName
+    ) {
+        return new Student(studentId, studentFirstName, studentLastName);
+    }
+}
