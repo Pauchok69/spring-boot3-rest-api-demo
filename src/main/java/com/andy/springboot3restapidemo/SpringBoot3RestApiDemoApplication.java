81f3d34252f8662b84496fdcdcd2cab5cae33a31
Andy <andrew.zerko@gmail.com>
2023-01-31 17:28:56 +0200

    Initial commit

---
+++ src/main/java/com/andy/springboot3restapidemo/SpringBoot3RestApiDemoApplication.java
@@ -0,0 +1,13 @@
+package com.andy.springboot3restapidemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot3RestApiDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot3RestApiDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/andy/springboot3restapidemo/SpringBoot3RestApiDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andy.springboot3restapidemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot3RestApiDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
